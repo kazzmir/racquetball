2bcb059eb0404c44dc1fc4a539d6473903636d40
Jon Rafkind <jon@rafkind.com>
2013-04-17 00:23:00 -0600

    set up some interfaces

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,48 @@
 #include <allegro5/allegro.h>
 #include <iostream>
 
+#include "pointer.h"
+
+namespace Physics{
+
+class Vector{
+public:
+    Vector(double x, double y, double z):
+    x(x), y(y), z(z){
+    }
+
+    Vector operator+(const Vector & what){
+        return Vector(x + what.x, y + what.y, z + what.z);
+    }
+
+    double x;
+    double y;
+    double z;
+};
+
+}
+
+namespace Racquetball{
+
+class Ball{
+public:
+    Physics::Vector position;
+    Physics::Vector move;
+};
+
+class Behavior{
+public:
+};
+
+class Player{
+public:
+    Util::ReferenceCount<Behavior> behavior;
+    Physics::Vector position;
+    Physics::Vector move;
+};
+
+}
+
 int main(){
     if (!al_init()){
         std::cout << "Could not initialize Allegro5" << std::endl;
